68b2d3e9f990bf00deda763e76e05b94879a5ab4
rafaelaandradec <rafaelaandrade.costa@gmail.com>
2022-01-20 22:59:46 -0300

    Adicionar arquivos de projeto.

---
+++ CursoO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//entrada de dados
+namespace CursoO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase, 3 cores e 3 cores seguida de espaço: ");
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            //split
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            //split
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
